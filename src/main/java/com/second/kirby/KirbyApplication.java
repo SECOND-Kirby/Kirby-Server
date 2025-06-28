d40a64dbf76114161b72be469cde20419b98a64b
Alal11 <spalqj56@naver.com>
2025-06-28 18:55:24 +0900

    init: Spring Boot 초기 세팅

---
+++ src/main/java/com/second/kirby/KirbyApplication.java
@@ -0,0 +1,13 @@
+package com.second.kirby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirbyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/second/kirby/KirbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.second.kirby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
