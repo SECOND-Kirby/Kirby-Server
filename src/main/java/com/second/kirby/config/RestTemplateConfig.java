e5844161b455b1d08904f9b0c2f67dc06541ff2c
Alal11 <spalqj56@naver.com>
2025-08-13 22:55:09 +0900

    feat: JWT 관련 설정 구현 #3

--- src/main/java/com/second/kirby/config/RestTemplateConfig.java
+++ src/main/java/com/second/kirby/config/RestTemplateConfig.java
@@ -1,8 +1,10 @@
 package com.second.kirby.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
 public class RestTemplateConfig {
 
     @Bean
